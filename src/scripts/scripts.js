e33b4a1c5beabdaa6f98a57904af9292fa24e7d3
Eduardo_Neves <eduardocn1999@gmail.com>
2020-02-12 22:03:06 -0300

    Basic template started. Included CSS

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,21 +1,9 @@
-document.getElementById('fetchProductDataBtn').addEventListener('click', fetchProductData);
 
-function fetchProductData(){
+function getData(){
     fetch('https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1')
     .then(response => response.json())
-    .then(products => console.log(products))
-    .then(products => {
-        let output = '<h2>Lists of Products</h2>';
-        output += '<ul>';
-        products.forEach(function(product) {
-            output += `
-                <li>
-                    ${product.name}
-                </li>
-            `;
-        });
-        output += '</ul>'
-        document.getElementById("response").innerHTML = output;
-    });
+    .then(products => console.log(products));
 }
+
+
 
