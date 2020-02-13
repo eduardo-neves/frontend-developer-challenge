5f7ddd25dca384d781a85c4d3e76296e04b70ef5
Eduardo_Neves <eduardocn1999@gmail.com>
2020-02-13 11:03:24 -0300

    Added function generateCard

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,5 +5,21 @@ function getData(){
     .then(products => console.log(products));
 }
 
+function generateCard(){
+    let output = `
+        <div id="card">
+            
+            <a href="https://placeholder.com"><img src="https://via.placeholder.com/200x150"></a>
+            <h2>Nome do produto</h2>
+            <p>Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
+            <p>De: R$23,99</p>
+            <h3>Por: R$19,99</h3>
+            <p>ou 2x de R$9,99</p>
+            <input id="BtnComprar" value="Comprar" type="button" onclick="">
+        </div>
+        `;
+
+        document.getElementById("response").innerHTML = output;
+}
 
 
