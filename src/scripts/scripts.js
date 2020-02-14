226bf9d63069a2010f26a0f94784c0e515f4bfd8
Eduardo_Neves <eduardocn1999@gmail.com>
2020-02-13 22:11:42 -0300

    Further developed template and functions.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,4 @@
+var output = "";
 
 function getData(){
     fetch('https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1')
@@ -5,21 +6,20 @@ function getData(){
     .then(products => console.log(products));
 }
 
-function generateCard(){
-    var template = `
+function generateCards(){
+    let template = `
         <div id="productCard">
-            
-            <a href="https://placeholder.com"><img src="https://via.placeholder.com/200x150"></a>
-            <h2>Nome do produto</h2>
+            <a  href="https://placeholder.com"><img id="productImage" src="https://via.placeholder.com/200x150"></a>
+            <h2 id="productName">Nome do produto</h2>
             <p>Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
             <p>De: R$23,99</p>
-            <h3>Por: R$19,99</h3>
+            <h3 id="productPrice">Por: R$19,99</h3>
             <p>ou 2x de R$9,99</p>
             <input id="BtnComprar" value="Comprar" type="button" onclick="">
         </div>
         `;
-        for(var i = 0; i<= 7; i++){
-           var output = output + template;
+        for(var i = 1; i<= 8; i++){
+           output = output + template;
           }
 
         document.getElementById("response").innerHTML = output;
