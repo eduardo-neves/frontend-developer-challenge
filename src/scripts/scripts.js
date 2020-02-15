94f3dee620d5d9328d4096f5699aa0b0c0120c5a
Eduardo_Neves <eduardocn1999@gmail.com>
2020-02-15 11:29:31 -0300

    Added comments. API implementation still pending

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,16 +1,16 @@
 var output = "";
 
-function getData(){
+function getData(){ //work in progress. will try to fetch data and then parse it.
     fetch('https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1')
     .then(response => response.json())
-    .then(products => console.log(products));
+    .then(products => console.log(products)); // response gets logged to console. seems fine.
 }
 
-function generateCards(){
+function generateCards(){ //html template for product cards.
     let template = `
         <div id="productCard">
             <a  href="https://placeholder.com"><img id="productImage" src="https://via.placeholder.com/200x150"></a>
-            <h2 id="productName">Nome do produto</h2>
+            <p id="productName">Nome do produto</p>
             <p>Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
             <p>De: R$23,99</p>
             <h3 id="productPrice">Por: R$19,99</h3>
@@ -19,10 +19,10 @@ function generateCards(){
         </div>
         `;
         for(var i = 1; i<= 8; i++){
-           output = output + template;
+           output = output + template; // for loop for creating the eight required cards.
           }
 
-        document.getElementById("response").innerHTML = output;
+        document.getElementById("response").innerHTML = output; //output gets display under the div id='response'.
 }
 
 
