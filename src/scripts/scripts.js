0c74050a223cacf5f9adc8507f78da08e5edba36
Eduardo_Neves <eduardocn1999@gmail.com>
2020-02-16 16:31:55 -0300

    More experimenting with API. No results

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,9 +1,13 @@
 var output = "";
 
 function getData(){ //work in progress. will try to fetch data and then parse it.
+    var promise = "Something";
     fetch('https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1')
-    .then(response => response.json())
-    .then(products => console.log(products)); // response gets logged to console. seems fine.
+    .then(response => response.text())
+    .then(products => console.log(products))
+    .then(products => promise = products); // response gets logged to console. seems fine.
+
+    document.getElementById("response").innerHTML = promise; //output gets display under the div id='response'.
 }
 
 function generateCards(){ //html template for product cards.
@@ -17,7 +21,7 @@ function generateCards(){ //html template for product cards.
             <p>ou 2x de R$9,99</p>
             <input id="BtnComprar" value="Comprar" type="button" onclick="">
         </div>
-        `;
+        `; // The template variable receives the basic structure of the card div.
         for(var i = 1; i<= 8; i++){
            output = output + template; // for loop for creating the eight required cards.
           }
